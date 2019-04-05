57ad4c69b7c90f5bf21aacde126f461d47f24e88
anisanisah05 <anisah.nabilah05@gmail.com>
2019-04-05 09:18:30 +0700

    done

---
+++ app/src/main/java/id/sch/smktelkom/www/datasiswa_nabil/AppDatabase.java
@@ -0,0 +1,9 @@
+package id.sch.smktelkom.www.datasiswa_nabil;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.RoomDatabase;
+
+@Database(entities = {SiswaModel.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract SiswaDao userDao();
+}
---
+++ app/src/main/java/id/sch/smktelkom/www/datasiswa_nabil/MyApp.java
@@ -0,0 +1,17 @@
+package id.sch.smktelkom.www.datasiswa_nabil;
+
+import android.app.Application;
+import android.arch.persistence.room.Room;
+
+public class MyApp extends Application {
+
+    public static AppDatabase db;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        db = Room.databaseBuilder(getApplicationContext(),
+                AppDatabase.class, "siswa.db").allowMainThreadQueries().build();
+    }
+
+}
